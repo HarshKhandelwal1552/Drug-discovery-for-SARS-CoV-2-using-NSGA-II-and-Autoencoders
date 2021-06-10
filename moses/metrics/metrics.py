61d52ffebd64f08cec755ef083f31710080a18bc
HarshKhandelwal1552 <63328220+HarshKhandelwal1552@users.noreply.github.com>
2021-06-10 12:53:13 +0530

    Update metrics.py

--- moses/metrics/metrics.py
+++ moses/metrics/metrics.py
@@ -88,7 +88,7 @@ def get_all_metrics(gen, k=None, n_jobs=1,
         else:
             pool = 1
     metrics['valid'] = fraction_valid(gen, n_jobs=pool)
-    print(gen.head())
+    print(gen)
     gen = remove_invalid(gen, canonize=True)
 
     print(len(gen))
