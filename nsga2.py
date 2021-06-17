c168a18111b9d94056ca4db251836ff6f7d21ea7
HarshKhandelwal1552 <63328220+HarshKhandelwal1552@users.noreply.github.com>
2021-06-17 14:14:12 +0530

    Update nsga2.py

--- nsga2.py
+++ nsga2.py
@@ -77,9 +77,10 @@ class NSGA2Utils:
         else:
             return -1
 
-    def create_children(self, population):
+    def create_children(self, population,  gen_i):
         children = []
-        with open('moses/dataset/data/dataset.smi') as f:
+        
+        with open('checkpoints/'+ + gen_i'.smi') as f:
             content = f.readlines()
         # you may also want to remove whitespace characters like `\n` at the end of each line
         smiles = [x.strip() for x in content] 
