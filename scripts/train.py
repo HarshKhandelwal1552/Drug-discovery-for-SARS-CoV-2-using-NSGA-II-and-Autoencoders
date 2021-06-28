d558b1d919109cb87e53c1ded0e33a6589fc7045
HarshKhandelwal1552 <63328220+HarshKhandelwal1552@users.noreply.github.com>
2021-06-28 18:13:39 +0530

    Update train.py

--- scripts/train.py
+++ scripts/train.py
@@ -58,9 +58,10 @@ def main(model, config):
     if config.vocab_save is not None:
         torch.save(vocab, config.vocab_save)
 
-    model = MODELS.get_model_class(model)(vocab, config).to(device)
     if config.pre_trained==1:
-        model.load( config.model_save)
+        model= torch.load( config.model_save).to(device)
+    else: model = MODELS.get_model_class(model)(vocab, config).to(device)
+
     trainer.fit(model, train_data, val_data)
 
     model = model.to('cpu')
